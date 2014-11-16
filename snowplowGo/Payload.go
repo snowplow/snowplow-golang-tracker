96880b03a1a2c15a9eb93a97565d1b301a8638a0
aalekh <aalekh1993@rediffmail.com>
2014-11-17 01:53:46 +0530

    Payload.go complete <Not Tested>

--- snowplowGo/Payload.go
+++ snowplowGo/Payload.go
@@ -4,6 +4,8 @@ import (
 	"net/url"
 	"net/http"
 	"time"
+	"encoding/base64"
+	"encoding/json"
 	)
 
 var TimeStamp string
@@ -19,4 +21,27 @@ func InitPayload() {
 		paraValue = ((int64)(time.Now()) - http.Server. ReadTimeout)*1000
 	}
 	Add("dtm", paraValue)	
+}
+
+func Add(name string, value int64) {
+	if value != nil and value != "" {
+		NameValuePair[name] = value	
+	}
+}
+
+func AddDict(dict){
+	for name,element := range dict{
+		Add(name, element)
+	}		
+}
+
+func AddJson(json map([string]string), Base64 bool, NameEncoded string, NameNotEncode string) {
+	if json != nil {
+		if Base64 {
+			Add(NameEncoded, b64.StdEncoding.EncodeToString(json.Marshal(json)))
+	}else{
+		Add(NameNotEncode, json.Marshal(json))
+
+	}
+
 }

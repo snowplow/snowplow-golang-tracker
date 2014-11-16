4eb540fce3faa6c49bbb89171e3d7cd77610d4e5
aalekh <aalekh1993@rediffmail.com>
2014-11-17 02:49:43 +0530

    Tested and created Subject.go file :D

---
+++ snowplowGo/Subject.go
@@ -0,0 +1,45 @@
+package snowplowGo
+
+import (
+	"strconv"
+	)
+
+const(
+	DEFAULT_PLATFORM = "srv"
+)
+
+var TrackerSettings map[string]string 
+
+func InitSubject() {
+	TrackerSettings["p"] = DEFAULT_PLATFORM
+}
+
+//Remember about getSubject as golang variable type access
+
+func SetPlatform(platform string) {
+	TrackerSettings["p"] = platform
+}
+
+func SetUserId(userId string) {
+	TrackerSettings["uid"] = userId
+}
+
+func SetScreenResolution(width int, height int) {
+	TrackerSettings["res"] = strconv.Itoa(width) + "x" + strconv.Itoa(height)
+}
+
+func SetViewPort(width int, height int) {
+	TrackerSettings["vp"] = strconv.Itoa(width) + "x" + strconv.Itoa(height)
+}
+
+func SetColorDepth(depth int) {
+	TrackerSettings["cd"] = strconv.Itoa(depth)
+}
+
+func SetTimeZone(timezone string) {
+	TrackerSettings["tz"] = timezone
+}
+
+func SetLanguage(language string) {
+	TrackerSettings["lang"] = language
+}

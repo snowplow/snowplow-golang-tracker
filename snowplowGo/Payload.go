dca56190f47e54fa63c89056312c6cee303a18d2
Aalekh Nigam <aalekh1993@rediffmail.com>
2014-12-03 19:11:13 +0530

    gofmt Payload.go

--- snowplowGo/Payload.go
+++ snowplowGo/Payload.go
@@ -1,45 +1,45 @@
 package snowplowGo
 
 import (
-	"net/url"
-	"net/http"
-	"time"
 	"encoding/base64"
 	"encoding/json"
-	)
+	"net/http"
+	"net/url"
+	"time"
+)
 
 var TimeStamp string
-TimeStamp = nil
 
-NameValuePair = make(map[string]int)
+var NameValuePair map[string]int
 var paraValue int64
 
 func InitPayload() {
-	if TimeStamp != nil{
-		paraValue = (int64)(TimeStamp)	
-	} else{
-		paraValue = ((int64)(time.Now()) - http.Server. ReadTimeout)*1000
+	if TimeStamp != nil {
+		paraValue = (int64)(TimeStamp)
+	} else {
+		paraValue = ((int64)(time.Now()) - http.Server.ReadTimeout) * 1000
 	}
-	Add("dtm", paraValue)	
+	Add("dtm", paraValue)
 }
 
 func Add(name string, value int64) {
-	if value != nil and value != "" {
-		NameValuePair[name] = value	
+	if value != nil && value != "" {
+		NameValuePair[name] = value
 	}
 }
 
-func AddDict(dict){
-	for name,element := range dict{
+func AddDict(dict) {
+	for name, element := range dict {
 		Add(name, element)
-	}		
+	}
 }
 
-func AddJson(json map([string]string), Base64 bool, NameEncoded string, NameNotEncode string) {
+func AddJson(json map[string]string, Base64 bool, NameEncoded string, NameNotEncode string) {
 	if json != nil {
 		if Base64 {
 			Add(NameEncoded, b64.StdEncoding.EncodeToString(json.Marshal(json)))
-	}else{
+		}
+	} else {
 		Add(NameNotEncode, json.Marshal(json))
 
 	}

42598fd9238ad7dbbbd3caa4e21708714f93e309
Aalekh Nigam <aalekh1993@rediffmail.com>
2014-12-06 03:43:05 +0530

    Added Comment's
    Few changes with parameter as emitter

--- snowplowGo/Emitter.go
+++ snowplowGo/Emitter.go
@@ -1,9 +1,26 @@
+/*
+Emitter.php
+Copyright (c) 2014 Snowplow Analytics Ltd. All rights reserved.
+This program is licensed to you under the Apache License Version 2.0,
+and you may not use this file except in compliance with the Apache License
+Version 2.0. You may obtain a copy of the Apache License Version 2.0 at
+http://www.apache.org/licenses/LICENSE-2.0.
+Unless required by applicable law or agreed to in writing,
+software distributed under the Apache License Version 2.0 is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+express or implied. See the Apache License Version 2.0 for the specific
+language governing permissions and limitations there under.
+Authors: Aalekh Nigam
+Copyright: Copyright (c) 2014 Snowplow Analytics Ltd
+License: Apache License Version 2.0
+*/
 package main
 
 import (
 	"net/url"
 )
 
+//Basic Schema Constants for specfying basic schemas for Events
 const (
 	DEFAULT_REQ_TYPE    = "POST"
 	DEFAULT_PROTOCOL    = "http"
@@ -11,8 +28,6 @@ const (
 	BASE_SCHEMA_PATH    = "iglu:com.snowplowanalytics.snowplow"
 	SCHEMA_TAG          = "jsonschema"
 )
-
-var emitter Emitter
 
 type Emitter struct {
 	PostRequestSchema string
@@ -23,6 +38,15 @@ type Emitter struct {
 	Buffer            []string
 	RequestsResult    []string
 }
+
+/**
+* Intitialize emitter to send event data to a collector
+*
+* @param string collectorUri - URI of the collector
+* @param string|null reqType - Request Type (POST or GET)
+* @param string|null protocol - Protocol to be used (HTTP or HTTPS)
+* @param int|null bufferSize - Buffer Size, amount of events to be stored before sending
+*/
 
 func (e *Emitter) InitEmitter(collectorUri string, reqType string, protocol string, bufferSize int) Emitter {
 
@@ -52,14 +76,23 @@ func (e *Emitter) InitEmitter(collectorUri string, reqType string, protocol stri
 	return emitter
 }
 
-func (e *Emitter) ReturnCollectorUrl(emitter Emitter, host string) url.URL {
-	switch emitter.reqType {
+ // Make Functions
+/**
+* Returns the collector URL based on: request type, protocol and host given
+* IF a bad type is given in emitter creation returns NULL
+*
+* @param string host - The Collector URI to be used for tracking
+* @return string|null collector_url - Returns the Collector URL
+*/
+
+func (e *Emitter) ReturnCollectorUrl( host string) url.URL {
+	switch e.reqType {
 	case "POST":
-		url = emitter.protocol + "://" + host + "/com.snowplowanalytics.snowplow/tp2"
+		url = e.protocol + "://" + host + "/com.snowplowanalytics.snowplow/tp2"
 		urlEncoded = url.Parse(url)
 		return urlEncoded
 	case "GET":
-		url = emitter.protocol + "://" + host + "/i?"
+		url = e.protocol + "://" + host + "/i?"
 		urlEncoded = url.Parse(url)
 		return urlEncoded
 	default:
@@ -68,13 +101,26 @@ func (e *Emitter) ReturnCollectorUrl(emitter Emitter, host string) url.URL {
 
 }
 
-func (e *Emitter) SendEvent(emitter Emitter, finalPayload string) {
-	Extend(emitter.Buffer, finalPayload)
-	if len(emitter.Buffer) >= emitter.BufferSize {
+ /**
+* Pushes the event payload into the emitter buffer.
+* When buffer is full it flushes the buffer.
+*
+* @param array finalPayload - Takes the Trackers Payload as a parameter
+*/
+
+func (e *Emitter) SendEvent( finalPayload []string) {
+	Extend(e.Buffer, finalPayload)
+	if len(e.Buffer) >= e.BufferSize {
 		Flush()
 	}
 
 }
+
+ /**
+* Flushes the event buffer of the emitter
+* Checks which send type the emitter is using and forwards data accordingly
+* Resets the buffer to nothing after flushing
+*/
 
 func (e *Emitter) Flush(emitter Emitter) {
 	if len(emitter.Buffer) != 0 {
@@ -90,29 +136,50 @@ func (e *Emitter) Flush(emitter Emitter) {
 	}
 }
 
-//Need to complete this function
-func (e *Emitter) GetRequest(data) {
+ // Send Functions
+/**
+* Using a GET Request sends the data to a collector
+*
+* @param array data - The array which is going to be sent in the GET Request
+*/
+func (e *Emitter) GetRequest(data []string) {
 	r := url.Get(HttpBuildQuery(data))
 	e.StoreRequestResults(r)
 }
 
-func (e *Emitter) PostRequest(data, emitter Emitter) {
+
+/**
+* Using a POST Request sends the data to a collector
+*
+* @param array data - Is the array which is going to be sent in the POST Request
+*/
+
+func (e *Emitter) PostRequest(data []string) {
 	m = make(map[string]string)
 	m["Content-Type"] = "application/json; charset=utf-8"
 	//post method to be made properly here
-	r := url.Post(emitter.CollectorUrl)
+	r := url.Post(e.CollectorUrl)
 	//
 	e.StoreRequestResults(r)
 }
 
-func (e *Emitter) ReturnPostRequest(emitter Emitter) []string {
+/**
+* Returns an array which has been formatted to be ready for a POST Request
+**/
+
+func (e *Emitter) ReturnPostRequest( ) []string {
 	dataPostRequest := make(map[string][]map[string]string)
-	dataPostRequest["schema"] = emitter.PostRequestSchema
-	for _, element := range emitter.Buffer {
+	dataPostRequest["schema"] = e.PostRequestSchema
+	for _, element := range e.Buffer {
 		append(dataPostRequest["data"], element)
 	}
 	return dataPostRequest
 }
+
+ /**
+* Stores all of the parameters of the Request Response into a Dynamic Array for use in unit testing.
+* @param Requests_Response r - Is the response from a GET or POST request
+**/
 
 func (e *Emitter) StoreRequestResults(r RequestsResponse) {
 	storeArray = make(map[string]string)

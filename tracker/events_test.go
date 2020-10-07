a6f37cd920c2ff80302871f2e74132044f84801e
Paul Boocock <paul@snowplowanalytics.com>
2020-10-07 11:07:35 +0100

    Add ability to set Subject per event (close #50)

--- tracker/events_test.go
+++ tracker/events_test.go
@@ -14,8 +14,9 @@
 package tracker
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPageViewInit(t *testing.T) {
@@ -33,6 +34,10 @@ func TestPageViewInit(t *testing.T) {
 	assert.NotEqual("", event.Timestamp)
 	assert.NotEqual("", event.EventId)
 	assert.Nil(event.TrueTimestamp)
+	assert.Nil(event.Subject)
+
+	subject := InitSubject()
+	subject.SetUserId("1234")
 
 	// Assert full init
 	event = PageViewEvent{
@@ -42,6 +47,7 @@ func TestPageViewInit(t *testing.T) {
 		Timestamp:     NewInt64(1234567890123),
 		EventId:       NewString("custom-uuid-string"),
 		TrueTimestamp: NewInt64(9876543210123),
+		Subject:       subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -51,7 +57,8 @@ func TestPageViewInit(t *testing.T) {
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
 	assert.Equal(int64(9876543210123), *event.TrueTimestamp)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"pv\",\"eid\":\"custom-uuid-string\",\"page\":\"Some Title\",\"refr\":\"google.com\",\"ttm\":\"9876543210123\",\"url\":\"http://acme.com\"}", event.Get().String())
+	assert.NotNil(event.Subject)
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"pv\",\"eid\":\"custom-uuid-string\",\"page\":\"Some Title\",\"refr\":\"google.com\",\"ttm\":\"9876543210123\",\"uid\":\"1234\",\"url\":\"http://acme.com\"}", event.Get().String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -82,6 +89,9 @@ func TestStructuredInit(t *testing.T) {
 	assert.NotEqual("", *event.EventId)
 	assert.Nil(event.TrueTimestamp)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = StructuredEvent{
 		Category:  NewString("category-1"),
@@ -91,6 +101,7 @@ func TestStructuredInit(t *testing.T) {
 		Value:     NewFloat64(123.456),
 		Timestamp: NewInt64(1234567890123),
 		EventId:   NewString("custom-uuid-string"),
+		Subject:   subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -101,7 +112,8 @@ func TestStructuredInit(t *testing.T) {
 	assert.Equal(float64(123.456), *event.Value)
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"se\",\"eid\":\"custom-uuid-string\",\"se_ac\":\"selected\",\"se_ca\":\"category-1\",\"se_la\":\"tshirts\",\"se_pr\":\"sell\",\"se_va\":\"123.46\"}", event.Get().String())
+	assert.NotNil(event.Subject)
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"se\",\"eid\":\"custom-uuid-string\",\"se_ac\":\"selected\",\"se_ca\":\"category-1\",\"se_la\":\"tshirts\",\"se_pr\":\"sell\",\"se_va\":\"123.46\",\"uid\":\"1234\"}", event.Get().String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -133,18 +145,22 @@ func TestSelfDescribingInit(t *testing.T) {
 	assert.NotEqual("", *event.EventId)
 	assert.Nil(event.TrueTimestamp)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = SelfDescribingEvent{
 		Event:     InitSelfDescribingJson("iglu:com.acme/event/jsonschema/1-0-0", map[string]string{"e": "acme"}),
 		Timestamp: NewInt64(1234567890123),
 		EventId:   NewString("custom-uuid-string"),
+		Subject:   subject,
 	}
 	event.Init()
 	assert.NotNil(event)
 	assert.Equal("{\"data\":{\"e\":\"acme\"},\"schema\":\"iglu:com.acme/event/jsonschema/1-0-0\"}", event.Event.String())
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"e\\\":\\\"acme\\\"},\\\"schema\\\":\\\"iglu:com.acme/event/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\"}", event.Get(false).String())
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"e\\\":\\\"acme\\\"},\\\"schema\\\":\\\"iglu:com.acme/event/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\",\"uid\":\"1234\"}", event.Get(false).String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -171,12 +187,16 @@ func TestScreenViewInit(t *testing.T) {
 	assert.NotEqual("", *event.EventId)
 	assert.Nil(event.TrueTimestamp)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = ScreenViewEvent{
 		Name:      NewString("some name"),
 		Id:        NewString("some id"),
 		Timestamp: NewInt64(1234567890123),
 		EventId:   NewString("custom-uuid-string"),
+		Subject:   subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -184,7 +204,7 @@ func TestScreenViewInit(t *testing.T) {
 	assert.Equal("some id", *event.Id)
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"id\\\":\\\"some id\\\",\\\"name\\\":\\\"some name\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/screen_view/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\"}", event.Get().Get(false).String())
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"id\\\":\\\"some id\\\",\\\"name\\\":\\\"some name\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/screen_view/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\",\"uid\":\"1234\"}", event.Get().Get(false).String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -214,6 +234,9 @@ func TestTimingInit(t *testing.T) {
 	assert.NotEqual("", *event.EventId)
 	assert.Nil(event.TrueTimestamp)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = TimingEvent{
 		Category:  NewString("some category"),
@@ -222,6 +245,7 @@ func TestTimingInit(t *testing.T) {
 		Label:     NewString("some label"),
 		Timestamp: NewInt64(1234567890123),
 		EventId:   NewString("custom-uuid-string"),
+		Subject:   subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -231,7 +255,7 @@ func TestTimingInit(t *testing.T) {
 	assert.Equal("some label", *event.Label)
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"category\\\":\\\"some category\\\",\\\"label\\\":\\\"some label\\\",\\\"timing\\\":12345678,\\\"variable\\\":\\\"some variable\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/timing/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\"}", event.Get().Get(false).String())
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"ue\",\"eid\":\"custom-uuid-string\",\"ue_pr\":\"{\\\"data\\\":{\\\"data\\\":{\\\"category\\\":\\\"some category\\\",\\\"label\\\":\\\"some label\\\",\\\"timing\\\":12345678,\\\"variable\\\":\\\"some variable\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/timing/jsonschema/1-0-0\\\"},\\\"schema\\\":\\\"iglu:com.snowplowanalytics.snowplow/unstruct_event/jsonschema/1-0-0\\\"}\",\"uid\":\"1234\"}", event.Get().Get(false).String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -280,6 +304,9 @@ func TestEcommerceTransactionInit(t *testing.T) {
 	assert.NotEqual("", event.EventId)
 	assert.Nil(event.TrueTimestamp)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = EcommerceTransactionEvent{
 		OrderId:     NewString("order-1"),
@@ -293,6 +320,7 @@ func TestEcommerceTransactionInit(t *testing.T) {
 		Currency:    NewString("EUR"),
 		Timestamp:   NewInt64(1234567890123),
 		EventId:     NewString("custom-uuid-string"),
+		Subject:     subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -308,7 +336,7 @@ func TestEcommerceTransactionInit(t *testing.T) {
 	assert.Nil(event.Items)
 	assert.Equal(int64(1234567890123), *event.Timestamp)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"tr\",\"eid\":\"custom-uuid-string\",\"tr_af\":\"coffee\",\"tr_ci\":\"Dijon\",\"tr_co\":\"France\",\"tr_cu\":\"EUR\",\"tr_id\":\"order-1\",\"tr_sh\":\"12.50\",\"tr_st\":\"Bourgogne\",\"tr_tt\":\"123456.79\",\"tr_tx\":\"222.35\"}", event.Get().String())
+	assert.Equal("{\"dtm\":\"1234567890123\",\"e\":\"tr\",\"eid\":\"custom-uuid-string\",\"tr_af\":\"coffee\",\"tr_ci\":\"Dijon\",\"tr_co\":\"France\",\"tr_cu\":\"EUR\",\"tr_id\":\"order-1\",\"tr_sh\":\"12.50\",\"tr_st\":\"Bourgogne\",\"tr_tt\":\"123456.79\",\"tr_tx\":\"222.35\",\"uid\":\"1234\"}", event.Get().String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -344,6 +372,9 @@ func TestEcommerceTransactionItemInit(t *testing.T) {
 	assert.Nil(event.Category)
 	assert.NotEqual("", *event.EventId)
 
+	subject := InitSubject()
+	subject.SetUserId("1234")
+
 	// Assert full init
 	event = EcommerceTransactionItemEvent{
 		Sku:      NewString("123-dda"),
@@ -352,6 +383,7 @@ func TestEcommerceTransactionItemInit(t *testing.T) {
 		Name:     NewString("power pc"),
 		Category: NewString("servers"),
 		EventId:  NewString("custom-uuid-string"),
+		Subject:  subject,
 	}
 	event.Init()
 	assert.NotNil(event)
@@ -361,7 +393,7 @@ func TestEcommerceTransactionItemInit(t *testing.T) {
 	assert.Equal("power pc", *event.Name)
 	assert.Equal("servers", *event.Category)
 	assert.Equal("custom-uuid-string", *event.EventId)
-	assert.Equal("{\"e\":\"ti\",\"eid\":\"custom-uuid-string\",\"ti_ca\":\"servers\",\"ti_nm\":\"power pc\",\"ti_pr\":\"123456.79\",\"ti_qu\":\"5000\",\"ti_sk\":\"123-dda\"}", event.Get().String())
+	assert.Equal("{\"e\":\"ti\",\"eid\":\"custom-uuid-string\",\"ti_ca\":\"servers\",\"ti_nm\":\"power pc\",\"ti_pr\":\"123456.79\",\"ti_qu\":\"5000\",\"ti_sk\":\"123-dda\",\"uid\":\"1234\"}", event.Get().String())
 
 	defer func() {
 		if err := recover(); err != nil {
@@ -384,4 +416,142 @@ func TestEcommerceTransactionItemInit(t *testing.T) {
 	}()
 	event = EcommerceTransactionItemEvent{}
 	event.Init()
+}
+
+func TestPageViewSubjectOverride(t *testing.T) {
+	assert := assert.New(t)
+
+	subject1 := InitSubject()
+	subject1.SetUserId("1234")
+	subject2 := InitSubject()
+	subject2.SetUserId("9876")
+
+	// Assert subject isn't overwritten
+	event := PageViewEvent{
+		PageUrl: NewString("http://acme.com"),
+		Subject: subject1,
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "1234")
+
+	// Assert subject is overwritten
+	event = PageViewEvent{
+		PageUrl: NewString("http://acme.com"),
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "9876")
+}
+
+func TestStructuredSubjectOverride(t *testing.T) {
+	assert := assert.New(t)
+
+	subject1 := InitSubject()
+	subject1.SetUserId("1234")
+	subject2 := InitSubject()
+	subject2.SetUserId("9876")
+
+	// Assert subject isn't overwritten
+	event := StructuredEvent{
+		Category: NewString("category-1"),
+		Action:   NewString("selected"),
+		Subject:  subject1,
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "1234")
+
+	// Assert subject is overwritten
+	event = StructuredEvent{
+		Category: NewString("category-1"),
+		Action:   NewString("selected"),
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "9876")
+}
+
+func TestSelfDescribingSubjectOverride(t *testing.T) {
+	assert := assert.New(t)
+
+	subject1 := InitSubject()
+	subject1.SetUserId("1234")
+	subject2 := InitSubject()
+	subject2.SetUserId("9876")
+
+	// Assert subject isn't overwritten
+	event := SelfDescribingEvent{
+		Event:   InitSelfDescribingJson("iglu:com.acme/event/jsonschema/1-0-0", map[string]string{"e": "acme"}),
+		Subject: subject1,
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "1234")
+
+	// Assert subject is overwritten
+	event = SelfDescribingEvent{
+		Event: InitSelfDescribingJson("iglu:com.acme/event/jsonschema/1-0-0", map[string]string{"e": "acme"}),
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "9876")
+}
+
+func TestEcommerceTransactionSubjectOverride(t *testing.T) {
+	assert := assert.New(t)
+
+	subject1 := InitSubject()
+	subject1.SetUserId("1234")
+	subject2 := InitSubject()
+	subject2.SetUserId("9876")
+
+	// Assert subject isn't overwritten
+	event := EcommerceTransactionEvent{
+		OrderId:    NewString("order-1"),
+		TotalValue: NewFloat64(123456.789),
+		Subject:    subject1,
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "1234")
+
+	// Assert subject is overwritten
+	event = EcommerceTransactionEvent{
+		OrderId:    NewString("order-1"),
+		TotalValue: NewFloat64(123456.789),
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "9876")
+}
+
+func TestEcommerceTransactionItemSubjectOverride(t *testing.T) {
+	assert := assert.New(t)
+
+	subject1 := InitSubject()
+	subject1.SetUserId("1234")
+	subject2 := InitSubject()
+	subject2.SetUserId("9876")
+
+	// Assert subject isn't overwritten
+	event := EcommerceTransactionItemEvent{
+		Sku:      NewString("123-dda"),
+		Price:    NewFloat64(123456.789),
+		Quantity: NewInt64(5000),
+		Subject:  subject1,
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "1234")
+
+	// Assert subject is overwritten
+	event = EcommerceTransactionItemEvent{
+		Sku:      NewString("123-dda"),
+		Price:    NewFloat64(123456.789),
+		Quantity: NewInt64(5000),
+	}
+	event.Init()
+	event.SetSubjectIfNil(subject2)
+	assert.Equal(event.Subject.Get()[UID], "9876")
 }
